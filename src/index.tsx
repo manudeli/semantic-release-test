b28be6c687914b2735f860fb06946749256d801f
마누 <manu.k@kakaocorp.com>
2022-05-19 03:42:27 +0900

    feat(semantic-release): major branch version test
    BREAKING CHANGE: description of breaking changes

--- src/index.tsx
+++ src/index.tsx
@@ -11,5 +11,5 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  * A custom Thing component. Neat!
  */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `version update test - minor branch`}</div>;
+  return <div>{children || `version update test - major branch`}</div>;
 };
