2e313b38032a534b51e3ecf81f57cac9f16ac919
마누 <manu.k@kakaocorp.com>
2022-05-21 14:45:16 +0900

    feat(semantic-release): hihi
    BREAKING_CHANGE: hihi

--- src/index.tsx
+++ src/index.tsx
@@ -11,5 +11,5 @@ export interface Props extends HTMLAttributes<HTMLDivElement> {
  * A custom Thing component. Neat!
  */
 export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `version update test - major branch`}</div>;
+  return <div>{children || `version update test - major branch hihihi`}</div>;
 };
